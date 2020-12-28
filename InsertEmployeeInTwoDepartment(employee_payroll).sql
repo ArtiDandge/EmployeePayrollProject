d8cc721e754ffe5cbdaee4de8a9be022e3b7e201
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-12-28 16:28:18 +0530

    [Arti Dandge] Add : Ability to make Terisa as part of Sales and Marking Department

---
+++ InsertEmployeeInTwoDepartment(employee_payroll).sql
@@ -0,0 +1,9 @@
+insert into employee_payroll
+(id, name, basic_pay, start_date, gender, address, phone_number, department, deduction, taxable_pay, income_tax, net_pay) values
+(105,'Terisa', 150000.00, '2019-12-03', 'F', 'Mumbai', '8884443332', 'Marketing', 20000.00, 10000.00, 200000.00, 300000.00);
+
+insert into employee_payroll
+(id, name, basic_pay, start_date, gender, address, phone_number, department, deduction, taxable_pay, income_tax, net_pay) values
+(106,'Terisa', 150000.00, '2019-12-03', 'F', 'Dubai', '723666632', 'Sales', 30000.00, 20000.00, 100000.00, 500000.00);
+
+
